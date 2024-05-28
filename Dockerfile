94ae83f87765f62a71b362c976949bdd373e9771
endaaman <ken@endaaman.me>
2024-05-28 16:04:17 +0900

    sveltekit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:20-slim
+
+ENV TZ Asia/Tokyo
+RUN npm install -g pnpm
+
+WORKDIR /app
+COPY package.json ./
+COPY pnpm-lock.yaml ./
+RUN pnpm install --frozen-lockfile
+
+COPY . .
+RUN npm run build
+
+ENV PUBLIC_UPSTREAM_URL_BASE http://blog-backend:3000
+
+EXPOSE 8000
+
+CMD ["npm", "run", "serve"]
---
+++ serve.js
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import { handler } from './build/handler.js'
+
+
+const app = express()
+
+const proxy = createProxyMiddleware({
+  target: process.env.PUBLIC_UPSTREAM_URL_BASE,
+  changeOrigin: true,
+})
+
+console.log(`PROXY TO: "${process.env.PUBLIC_UPSTREAM_URL_BASE}"`, )
+
+const port = process.env.PORT || 8000
+
+app.use('/api/*', proxy)
+app.use('/static/*', proxy)
+app.use(handler)
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
